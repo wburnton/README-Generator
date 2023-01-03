08776952a485c8fa88a2b5b308c710526636e41d
Will Burnton <willburnton@Wills-MacBook-Air.local>
2023-01-03 14:17:16 -0600

    readme test

---
+++ Develop/README2.md
@@ -0,0 +1,42 @@
+# lala 
+
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+## Description 
+
+lalal 
+
+## Table of Contents (Optional)
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license) 
+- [Test](#test) 
+- [Questions](#questions)
+
+## Installation
+
+stuff
+## Usage
+
+more stuff
+
+## Credits
+
+lots of stuff 
+
+## Tests 
+
+boo
+
+## License
+
+MIT
+
+## Badges
+
+## Questions 
+
+wburnton 
+email@email.com;
--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,135 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application 
+const inquirer = require('inquirer');
+const fs = require('fs');  
+const generateReadMe = ({ title, description, instructions, usage, contribution, test, license, github, email}) => 
+`# ${title} 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+${licenseBadge(license)}
+## Description 
 
-// TODO: Create a function to write README file
+${description} 
+
+## Table of Contents (Optional)
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license) 
+- [Test](#test) 
+- [Questions](#questions)
+
+## Installation
+
+${instructions}
+## Usage
+
+${usage}
+
+## Credits
+
+${contribution} 
+
+## Tests 
+
+${test}
+
+## License
+
+${license}
+
+## Badges
+
+## Questions 
+
+${github} 
+${email};`
+
+function licenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+};
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `https://lbesson.mit-license.org/`
+  }
+  if (license === 'GPL') {
+    return `http://perso.crans.org/besson/LICENSE.html`
+  }
+  if (license === 'CC--0') {
+    return `https://creativecommons.org/licenses/by-nd/4.0` 
+  }
+};
+
+
+// TODO: Create an array of questions for user input 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your Project Title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of your project',
+    },
+    {
+      type: 'input',
+      name: 'instructions',
+      message: 'What are the instructions for installation of your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter information on the usage of your project',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Enter the contribution guidelines',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Enter the test instructions for your project.', 
+    }, 
+    {
+        type: 'multi-choice',
+        name: 'license',
+        message: 'Choose license for project.', 
+        choices: ['MIT', 'Apache License', 'GNU General Public License', 'Boost Software License', 'The Unlicense' ]
+    }, 
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your github username', 
+    }, 
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your Email address?', 
+    },
+  ])
+  .then((answers) => { 
+    const readmePageContent = generateReadMe(answers); 
+  
+    fs.writeFile('README2.md', readmePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!'));
+});
+const questions = []; 
+
+// TODO: Create a function to write README file 
+
+
+
 function writeToFile(fileName, data) {}
+
+
 
 // TODO: Create a function to initialize app
 function init() {}
